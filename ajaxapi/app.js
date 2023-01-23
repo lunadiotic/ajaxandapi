da92fbc9c3a693a8cbb6e826228a908a7c14a8d0
aim <ahmadhakim404@gmail.com>
2023-01-23 18:21:18 +0700

    first init

---
+++ ajaxapi/app.js
@@ -0,0 +1,12 @@
+const data = `{
+    "ticker": {
+    "base": "BTC",
+    "target": "IDR",
+    "price": "345001000.00000000",
+    "volume": "28.02287745",
+    "change": "-430000.00000000"
+    },
+    "timestamp": 1674472505,
+    "success": true,
+    "error": ""
+    }`;
