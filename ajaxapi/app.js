a5e6bef7bdc451de913e1c6aa67cd484a8781cda
aim <ahmadhakim404@gmail.com>
2023-01-27 19:24:12 +0700

    request menggunakan axios

--- ajaxapi/app.js
+++ ajaxapi/app.js
@@ -39,17 +39,49 @@
 // 		console.log('error', err);
 // 	});
 
-const loadPeople = async () => {
+// const loadPeople = async () => {
+// 	try {
+// 		const res = await fetch('https://swapi.dev/api/peoplasjkdge/1');
+// 		const data = await res.json();
+// 		console.log(data);
+// 		const res2 = await fetch('https://swapi.dev/api/people/2');
+// 		const data2 = await res2.json();
+// 		console.log(data2);
+// 	} catch (err) {
+// 		console.log('error', err);
+// 	}
+// };
+
+// loadPeople();
+
+// axios
+// 	.get('https://swapi.dev/api/people123/1')
+// 	.then((res) => {
+// 		console.log(res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 		alert(err.message);
+// 	});
+
+// axios
+// 	.get('https://swapi.dev/api/people123/1')
+// 	.then((res) => {
+// 		console.log(res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 		alert(err.message);
+// 	});
+
+const getPeople = async (id) => {
 	try {
-		const res = await fetch('https://swapi.dev/api/peoplasjkdge/1');
-		const data = await res.json();
-		console.log(data);
-		const res2 = await fetch('https://swapi.dev/api/people/2');
-		const data2 = await res2.json();
-		console.log(data2);
-	} catch (err) {
-		console.log('error', err);
+		const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+		console.log(res.data);
+	} catch (error) {
+		console.log(error);
+		console.log(error.message);
+		console.log(error.response.status);
+		console.log(error.response.data);
 	}
 };
-
-loadPeople();
