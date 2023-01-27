e78d353382f7d19e32198adf8b2bb560d05e0920
aim <ahmadhakim404@gmail.com>
2023-01-28 02:01:07 +0700

    update studi kasus

--- tvshow/app.js
+++ tvshow/app.js
@@ -1,14 +1,20 @@
 const form = document.querySelector('#search-form');
+
 form.addEventListener('submit', async (e) => {
 	e.preventDefault();
-	const searchQuery = form.elements.query.value;
-	const config = { params: { q: searchQuery } };
+
+	document.querySelectorAll('img').forEach((img) => img.remove());
+
+	const keyword = form.elements.query.value;
+	const config = {
+		params: { q: keyword },
+	};
 	const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-	showImages(res.data);
+	getImages(res.data);
 	form.elements.query.value = '';
 });
 
-const showImages = (shows) => {
+const getImages = (shows) => {
 	for (let result of shows) {
 		if (result.show.image) {
 			const img = document.createElement('img');
